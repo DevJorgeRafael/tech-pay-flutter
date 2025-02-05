dca4e4674a939fdd91e132bd6d8c76d66fbb12e2
Rafael2408 <rafaelrosero.r.r2408@gmail.com>
2025-02-05 16:26:26 -0500

    filtrar por fecha

--- lib/features/home/presentation/widgets/build_filtro_por_fechat.dart
+++ lib/features/home/presentation/widgets/build_filtro_por_fechat.dart
@@ -2,57 +2,137 @@ import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:provider/provider.dart';
 import 'package:tech_pay/features/home/presentation/providers/transacciones_provider.dart';
+import 'package:tech_pay/features/home/presentation/widgets/build_transaccion_card.dart';
 
-Widget buildFiltroPorFecha(BuildContext context) {
-  return Center(
-    child: ElevatedButton(
-      onPressed: () async {
-        DateTimeRange? pickedRange = await showDateRangePicker(
-          context: context,
-          firstDate: DateTime(2020),
-          lastDate: DateTime.now(),
-        );
+class FiltroPorFechaWidget extends StatefulWidget {
+  @override
+  _FiltroPorFechaWidgetState createState() => _FiltroPorFechaWidgetState();
+}
 
-        if (pickedRange != null) {
-          final transaccionesProvider =
-              Provider.of<TransaccionesProvider>(context, listen: false);
-          final transaccionesFiltradas =
-              transaccionesProvider.transacciones.where((t) {
-            return t.transaccionFecha.isAfter(pickedRange.start) &&
-                t.transaccionFecha.isBefore(pickedRange.end);
-          }).toList();
+class _FiltroPorFechaWidgetState extends State<FiltroPorFechaWidget> {
+  DateTimeRange? _selectedRange;
 
-          showDialog(
-            context: context,
-            builder: (context) {
-              return AlertDialog(
-                title: const Text("Transacciones Filtradas"),
-                content: SizedBox(
-                  height: 300,
-                  child: ListView.builder(
-                    itemCount: transaccionesFiltradas.length,
-                    itemBuilder: (context, index) {
-                      final transaccion = transaccionesFiltradas[index];
-                      return ListTile(
-                        title: Text(
-                            "${transaccion.transaccionMonto} ${transaccion.transaccionMoneda}"),
-                        subtitle: Text(
-                            "Fecha: ${DateFormat('dd MMM yyyy', 'es_ES').format(transaccion.transaccionFecha)}"),
-                      );
-                    },
-                  ),
-                ),
-                actions: [
-                  TextButton(
-                      onPressed: () => Navigator.pop(context),
-                      child: const Text("Cerrar")),
-                ],
+  // Formato de fecha
+  String get _fechaFormateada {
+    if (_selectedRange == null) {
+      return "Seleccione un rango de fechas";
+    }
+    return "${DateFormat('dd MMM yyyy').format(_selectedRange!.start)} - ${DateFormat('dd MMM yyyy').format(_selectedRange!.end)}";
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Builder(
+          builder: (context) => GestureDetector(
+            onTap: () async {
+              DateTimeRange? pickedRange = await showDateRangePicker(
+                context: context,
+                firstDate: DateTime(2020),
+                lastDate: DateTime.now(),
+                builder: (context, child) {
+                  return Theme(
+                    data: ThemeData.light().copyWith(
+                      primaryColor: Colors.deepPurple,
+                      colorScheme:
+                          const ColorScheme.light(primary: Colors.deepPurple),
+                      buttonTheme: const ButtonThemeData(
+                          textTheme: ButtonTextTheme.primary),
+                    ),
+                    child: child!,
+                  );
+                },
               );
+
+              if (pickedRange != null) {
+                setState(() {
+                  _selectedRange = pickedRange;
+                });
+              }
             },
-          );
-        }
+            child: Container(
+              padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 16),
+              margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(12),
+                border: Border.all(color: Colors.grey.shade300),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black.withOpacity(0.05),
+                    blurRadius: 8,
+                    spreadRadius: 2,
+                    offset: const Offset(0, 3),
+                  ),
+                ],
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    _fechaFormateada,
+                    style: const TextStyle(fontSize: 16, color: Colors.black54),
+                  ),
+                  const Icon(Icons.calendar_today, color: Colors.deepPurple),
+                ],
+              ),
+            ),
+          ),
+        ),
+        const SizedBox(
+            height: 10), // Espacio para separar el selector de fechas
+        Expanded(
+          child: _buildTransaccionesFiltradas(context),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildTransaccionesFiltradas(BuildContext context) {
+    final transaccionesProvider =
+        Provider.of<TransaccionesProvider>(context, listen: false);
+
+    // Evita el error asegurando que _selectedRange no sea null antes de filtrar
+    if (_selectedRange == null) {
+      return const Center(
+        child: Text(
+          "Seleccione un rango de fechas",
+          style: TextStyle(fontSize: 16, color: Colors.black54),
+        ),
+      );
+    }
+
+    final transaccionesFiltradas =
+        transaccionesProvider.transacciones.where((t) {
+      return t.transaccionFecha.isAfter(
+              _selectedRange!.start.subtract(const Duration(seconds: 1))) &&
+          t.transaccionFecha
+              .isBefore(_selectedRange!.end.add(const Duration(seconds: 1)));
+    }).toList();
+
+    if (transaccionesFiltradas.isEmpty) {
+      return const Center(
+        child: Padding(
+          padding: EdgeInsets.all(16),
+          child: Text(
+            "No hay transacciones en este rango de fechas",
+            style: TextStyle(fontSize: 16, color: Colors.black54),
+            textAlign: TextAlign.center,
+          ),
+        ),
+      );
+    }
+
+    return ListView.builder(
+      physics: const BouncingScrollPhysics(),
+      padding: const EdgeInsets.symmetric(horizontal: 16),
+      itemCount: transaccionesFiltradas.length,
+      itemBuilder: (context, index) {
+        final transaccion = transaccionesFiltradas[index];
+        return BuildTransaccionCard(transaccion); 
       },
-      child: const Text("Seleccionar Rango de Fechas"),
-    ),
-  );
+    );
+  }
 }
